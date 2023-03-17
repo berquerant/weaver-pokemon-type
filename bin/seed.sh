e03defdd677711a29d08c0aeeb923bb9d27e3362
berquerant <sin@sin-pro.local>
2023-03-17 09:51:10 +0900

    Add --protocol=tcp option

--- bin/seed.sh
+++ bin/seed.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 set -x
+
 d=$(cd $(dirname $0)/..; pwd)
-mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASS" "$MYSQL_DB" --connect-timeout=3 < "${d}/mysql/seed.sql"
+mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASS" "$MYSQL_DB" --connect-timeout=3 --protocol=tcp < "${d}/mysql/seed.sql"
